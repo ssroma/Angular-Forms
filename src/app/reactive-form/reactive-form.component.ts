92d214e8dd796ac666b2bc26e36f3415a429576e
Sergio Roma <roma.sergio@gmail.com>
2019-03-16 18:40:05 +0000

    Hobbies field add with ArrayGroup.

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -9,13 +9,56 @@ import { NgForm } from '@angular/forms';
 export class ReactiveFormComponent implements OnInit {
 
   genders = [ 'male', 'female'];
+  
 
-  constructor() { }
+  signupForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit( ) {
+    // this.signupForm = new FormGroup({
+    //   'username': new FormControl(null, [Validators.required, Validators.minLength(3)] ),
+    //   'email' : new FormControl(null, [Validators.required, Validators.email ] ),
+    //   'gender' : new FormControl('male')
+    // });
+    this.signupForm = this.fb.group({
+      userData : this.fb.group({ 
+        username : [null, [Validators.required, Validators.minLength(3)] ],
+        email : [null, [Validators.required, Validators.email] ],
+      }),
+      
+      gender : ['male' ],
+      emailsList : this.fb.array([])
+    })
   }
 
-  onSubmit(form: NgForm){
-    console.log(form);
+  get userData(){
+    return this.signupForm.get('userData');
   }
+
+  get username(){
+   // let userGroup = <FormGroup>this.userData;
+    return this.userData.get('username');
+  }
+  get email(){
+    return this.userData.get('email');
+  }
+
+  get emailsList(){
+    return this.signupForm.get('emailsList') as FormArray;
+  }
+
+  addEmails(){
+    let control = new FormControl(null, Validators.required);
+    //let control = this.fb.group({ emails : [null, Validators.required]});
+   //(<FormArray>this.signupForm.get('emailsList')).push(control);
+   this.emailsList.push(control);
+  }
+
+  onSubmit( ){
+    console.log( this.signupForm );
+    //console.log(this.userData);
+  }
+
+  
 }
